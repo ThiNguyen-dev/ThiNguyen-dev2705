742628d2cc8b2ea953b5cfe566b318d9f2687f0f
Nguyen Thi Truc Thi <89916739+ThiNguyen-dev@users.noreply.github.com>
2021-10-04 15:04:22 +0700

    Add files via upload

---
+++ Form DangNhap/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Form_DangNhap
+{
+    partial class FrmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_TDN = new System.Windows.Forms.Label();
+            this.txt_TenDangNhap = new System.Windows.Forms.TextBox();
+            this.lbl_MatKhau = new System.Windows.Forms.Label();
+            this.txt_MatKhau = new System.Windows.Forms.TextBox();
+            this.btn_DongY = new System.Windows.Forms.Button();
+            this.btn_BoQua = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_TDN
+            // 
+            this.lbl_TDN.AutoSize = true;
+            this.lbl_TDN.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbl_TDN.Location = new System.Drawing.Point(56, 112);
+            this.lbl_TDN.Name = "lbl_TDN";
+            this.lbl_TDN.Size = new System.Drawing.Size(162, 26);
+            this.lbl_TDN.TabIndex = 0;
+            this.lbl_TDN.Text = "Tên đăng nhập:";
+            // 
+            // txt_TenDangNhap
+            // 
+            this.txt_TenDangNhap.Location = new System.Drawing.Point(236, 111);
+            this.txt_TenDangNhap.Name = "txt_TenDangNhap";
+            this.txt_TenDangNhap.Size = new System.Drawing.Size(338, 26);
+            this.txt_TenDangNhap.TabIndex = 1;
+            // 
+            // lbl_MatKhau
+            // 
+            this.lbl_MatKhau.AutoSize = true;
+            this.lbl_MatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbl_MatKhau.Location = new System.Drawing.Point(56, 186);
+            this.lbl_MatKhau.Name = "lbl_MatKhau";
+            this.lbl_MatKhau.Size = new System.Drawing.Size(107, 26);
+            this.lbl_MatKhau.TabIndex = 2;
+            this.lbl_MatKhau.Text = "Mật khẩu:";
+            // 
+            // txt_MatKhau
+            // 
+            this.txt_MatKhau.Location = new System.Drawing.Point(236, 188);
+            this.txt_MatKhau.Name = "txt_MatKhau";
+            this.txt_MatKhau.Size = new System.Drawing.Size(338, 26);
+            this.txt_MatKhau.TabIndex = 3;
+            // 
+            // btn_DongY
+            // 
+            this.btn_DongY.Location = new System.Drawing.Point(397, 308);
+            this.btn_DongY.Name = "btn_DongY";
+            this.btn_DongY.Size = new System.Drawing.Size(126, 42);
+            this.btn_DongY.TabIndex = 4;
+            this.btn_DongY.Text = "Đồng ý";
+            this.btn_DongY.UseVisualStyleBackColor = true;
+            this.btn_DongY.Click += new System.EventHandler(this.btn_DongY_Click);
+            // 
+            // btn_BoQua
+            // 
+            this.btn_BoQua.Location = new System.Drawing.Point(581, 308);
+            this.btn_BoQua.Name = "btn_BoQua";
+            this.btn_BoQua.Size = new System.Drawing.Size(126, 42);
+            this.btn_BoQua.TabIndex = 5;
+            this.btn_BoQua.Text = "Bỏ qua";
+            this.btn_BoQua.UseVisualStyleBackColor = true;
+            this.btn_BoQua.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_BoQua);
+            this.Controls.Add(this.btn_DongY);
+            this.Controls.Add(this.txt_MatKhau);
+            this.Controls.Add(this.lbl_MatKhau);
+            this.Controls.Add(this.txt_TenDangNhap);
+            this.Controls.Add(this.lbl_TDN);
+            this.Name = "Form1";
+            this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_TDN;
+        private System.Windows.Forms.TextBox txt_TenDangNhap;
+        private System.Windows.Forms.Label lbl_MatKhau;
+        private System.Windows.Forms.TextBox txt_MatKhau;
+        private System.Windows.Forms.Button btn_DongY;
+        private System.Windows.Forms.Button btn_BoQua;
+    }
+}
+
---
+++ Form DangNhap/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_DangNhap
+{
+    public partial class FrmDangNhap : Form
+    {
+        public FrmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_DongY_Click(object sender, EventArgs e)
+        {
+            if(this.txt_TenDangNhap.Text == "ndungithue" && this.txt_MatKhau.Text == "Abc@123")
+            {
+                MessageBox.Show("Đang nhập thành công");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
